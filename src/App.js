2ab478cd6415afb9612ef5842f2f4207b8aca2ac
SeanKim <sean.kim84@gmail.com>
2018-11-03 10:42:49 +0700

    First Commit(Configuration)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ letterSpacing: "0.3rem",fontFamily: 'Spicy Rice' }}>ReduxPhotography</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
